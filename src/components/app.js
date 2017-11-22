6023997ba9f67c74291f5359f538af2e62897d22
niklas-palm <niklas-palm@hotmail.com>
2017-11-22 17:13:40 +0100

    second commit

--- src/components/app.js
+++ src/components/app.js
@@ -1,24 +1,14 @@
 import React, { Component } from 'react';
 
-<<<<<<< HEAD
 import SearchBar from '../containers/search_bar';
 import WeatherList from '../containers/weather_list';
-=======
-import BookList from '../containers/book-list';
-import BookDetail from '../containers/book-detail';
->>>>>>> 923054e79dfc40e8e4329b77d79d001b9d9e8bac
 
 export default class App extends Component {
   render() {
     return (
       <div>
-<<<<<<< HEAD
         <SearchBar />
         <WeatherList/>
-=======
-        <BookList />
-        <BookDetail />
->>>>>>> 923054e79dfc40e8e4329b77d79d001b9d9e8bac
       </div>
     );
   }
